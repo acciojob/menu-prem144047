6aa1a6b5b2687fda320fe3938bf1af8f94cc54c5
Prem Kumar <prem144047@gmail.com>
2026-01-31 21:25:44 +0530

    Refactor Categories component and remove App logic

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,51 +1,25 @@
-import React, { useState } from "react";
-import items from "./data";
-import Categories from "./Categories";
-import Menu from "./Menu"
-
-import '../styles/App.css';
-
-const allCategories = ["all","breakfast","lunch","shakes"]
-// const allCat = items.map(item=>item.category)
-// const tempSet = new Set(allCat)
-// let tempArr = [...tempSet]
-
-// 1st way
-// const finalArr = ["all",...tempArr]
-
-//2nd way
-// tempArr.unshift("all")
-// console.log(tempArr,finalArr)
-
-// const allCategories = ["all",...new Set(items.map((item)=>item.category))]
-
-const App = () => {
-
-    const [menuItems,setMenuItems] = useState(items)
-    const [categories,setCategories] = useState(allCategories)
+import React from "react"
 
 
-    const filterItems = (categoryName) => {
-        if(categoryName === "all"){
-            setMenuItems(items)
-        }else{
-            const newItems = items.filter((item)=>item.category===categoryName)
-            setMenuItems(newItems)
-        }
-    }
+const Categories = ({categories,filterItems}) => {
+
     return(
-        <main id="main">
-            <section className="menu section">
-                <div className="title">
-                    <h2>Our Menu</h2>
-                    <div className="underline"></div>    
-                </div>
-                <Categories categories={categories} filterItems={filterItems}/>
-                <Menu items={menuItems} />
-            </section>
-        </main>
+        <div className="btn-container">
+            {
+                categories.map((category,index)=>{
+                    return(
+                        <button id={`filter-btn-${index}`}
+                            type="button"
+                            className="filter-btn"
+                            key={index}
+                            onClick={()=>filterItems(category)}>
+                                {category}
+                        </button>
+                    )
+                })
+            }    
+        </div>
     )
-
 }
 
-export default App;
+export default Categories
